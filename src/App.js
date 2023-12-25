96e5dd538a8637547da13d26b46913ff2094da03
kthurman59 <kthurman59@gmail.com>
2023-12-25 07:07:55 -0600

    button logic update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 // App.js
-import React from 'react';
+import React, { useState } from 'react';
 import TimerButton from './TimerButton'; // Import the TimerButton component
 
 const App = () => {
+  const [displayString, setDisplayString] = useState(''); // State to hold the display string
+
+  // Function to update the displayed string
+  const updateDisplayString = () => {
+    setDisplayString('Your string here'); // Update the string as needed
+  };
+
   return (
     <div>
       <h1>CLICK BUTTON FOR A PROBLEM</h1>
-      <TimerButton /> {/* Render the TimerButton component */}
+      <TimerButton onClick={updateDisplayString} /> {/* Pass a function to update the string */}
+      <div>{displayString}</div> {/* Display the string in a div */}
     </div>
   );
 };
