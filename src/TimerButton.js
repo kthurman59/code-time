da76b172ff3eeac2557db4444e87ac50dc6138d9
kthurman59 <kthurman59@gmail.com>
2023-12-25 05:20:26 -0600

    UPDATING BUTTON

---
+++ src/TimerButton.js
@@ -0,0 +1,13 @@
+// TimerButton.js
+
+import React from 'react';
+
+const TimerButton = () => {
+    return (
+        <button style={{ fontSize: '1.5rem', padding: '10px 20px' }}>
+            5 Mins
+        </button>
+    );
+};
+
+export default TimerButton;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Import the main App component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App /> {/* Render the App component */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
